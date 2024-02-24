2e595fbb5c60a44cea3d9ee1ad91d7d8e5945925
Song Eating <songeating@outlook.com>
2024-02-24 20:34:07 +0800

    first commit

---
+++ flt.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import sys
+from bs4 import BeautifulSoup
+
+save_dir = "/sdcard/Download/flt/"
+
+
+def doimgs(bs):
+    divs = bs.find_all('div')
+    for div in divs:
+        # Get img url
+        imgs = BeautifulSoup(str(div), features='html.parser').find_all('img')
+        if len(imgs) > 0:
+            # Download and save
+            img_url = imgs[0].get('src')
+            resp = requests.get(url=img_url)
+            file_name = img_url.split('/')[-1]
+            file_path = os.path.join(save_dir, file_name)
+            open(file_path, 'wb').write(resp.content)
+            print(f'Downloaded: {file_name}')
+
+
+def doit(u):
+    print(f'Parse: {u}')
+    html = requests.get(url=u).text
+    bs = BeautifulSoup(html, features='html.parser')
+    # Find div.content_left
+    divs = bs.find_all('div', class_='content_left')
+    # Do imgs downloaded
+    doimgs(BeautifulSoup(str(divs[0]), features='html.parser'))
+    # Get URL of next page
+    divs_page = BeautifulSoup(str(divs[1]), features='html.parser').find_all('div', class_='page')
+    next_url = 'https://fulitu.me' + BeautifulSoup(str(divs_page[0]), features='html.parser').find_all('a')[-1].get('href')
+    # Re-doit!
+    if u == next_url:
+        return
+    else:
+        doit(next_url)
+
+
+
+first_url = sys.argv[1]
+save_dir = os.path.join(save_dir, BeautifulSoup(requests.get(url=first_url).text, features='html.parser').find('h1').string)
+print(f'Save to: {save_dir}')
+os.makedirs(save_dir)
+doit(first_url)
+print(f'Completed: {save_dir}')
---
+++ gflt.py
@@ -0,0 +1,69 @@
+import os
+
+USERNAME = 'songeating'
+REPO = 'beauty'
+BASEURL = f'https://mirror.ghproxy.com/https://raw.githubusercontent.com/{USERNAME}/{REPO}/master/flt/'
+REPO_URL = f'https://github.com/{USERNAME}/{REPO}'
+
+
+def gpages(base):
+    mainpage_body = ''
+    # Every directory is a collection of images
+    for dir in os.listdir(base):
+        dir_path = os.path.join(base, dir)
+        os.mkdir(dir)
+        subpage_body = ''
+        # Mark the first image
+        first = True
+        for root, ds, fs in os.walk(dir_path):
+            # Every 'f' is an image
+            for f in fs:
+                img_src = BASEURL + os.path.join(dir, f)
+                subpage_body += f'<img src="{img_src}">'
+                if first:
+                    first = False
+                    # Add collection to mainpage_body
+                    sub_url = BASEURL + dir
+                    mainpage_body += f'<a href="{sub_url}"><div><img class="cover" src="{img_src}"><div>{dir}</div></div></a><br><br>'
+        # Generate subpage
+        subpage_html = f'''<html>
+<head>
+<title>{dir}</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<style>
+img {{
+    width: 100%;
+}}
+</style>
+</head>
+<body>
+<h1>{dir}</h1>
+{subpage_body}
+</body>
+</html>'''
+        open(os.path.join(dir, 'index.html'), 'w').write(subpage_html)
+    # Generate mainpage
+    mainpage_html = f'''<html>
+<head>
+<title>Beauty</title>
+<p>GitHub: <a href="{REPO_URL}">{USERNAME}/{REPO}</a></p>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<style>
+img.cover {{
+    width: 100%;
+}}
+</style>
+</head>
+<body>
+<h1>Beauty</h1>
+{mainpage_body}
+</body>
+</html>'''
+    open('index.html', 'w').write(mainpage_html)
+
+
+#base = '/sdcard/Download/flt'
+base = './flt'
+gpages(base)
